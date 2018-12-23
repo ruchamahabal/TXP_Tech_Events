9f7a2c79c94e9662d65be4f433fc598da3400339
ruchamahabal <ruchamahabal2@gmail.com>
2018-12-23 21:31:04 +0530

    error_hunt q3

---
+++ error_hunt/q3/q3_correct.c
@@ -0,0 +1,68 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char text[100];
+    int i;
+    int countL,countU,countS;
+
+    printf("Enter any string: ");
+    gets(text);
+
+    //here, we are printing string using printf
+    //without using loop
+    printf("Entered string is: %s\n",text);
+
+    //count lower case, upper case and special characters
+    //assign 0 to counter variables
+    countL=countU=countS=0;
+
+    for(i=0;text[i]!='\0';i++)
+    {
+        //check for alphabet
+        if((text[i]>='A' && text[i]<='Z') || (text[i]>='a' && text[i]<='z'))
+        {
+            if((text[i]>='A' && text[i]<='Z'))
+            {
+                //it is upper case alphabet
+                countU++;
+            }
+            else
+            {
+                //it is lower case character
+                countL++;
+            }
+        }
+        else
+        {
+            //character is not an alphabet
+            countS++; //it is special character
+        }
+    }
+
+    //print values
+    printf("Upper case characters: %d\n",countU);
+    printf("Lower case characters: %d\n",countL);
+    printf("Special characters: %d\n",countS);
+
+    return 0;
+}
+
+/*sample expected output
+Enter any string: Hello Friends, I am Mike.
+Entered string is: Hello Friends, I am Mike.
+Upper case characters: 4
+Lower case characters: 15
+Special characters: 6
+*/
+
+/*
+1.gets
+2.print in place of printf
+3.characters dont have inverted commas
+4.text[i]
+5.count+++
+6. line number 35 %s in place of %d
+7. return statement
+*/
