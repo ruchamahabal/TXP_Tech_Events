96f4f0ac203dca8537c990fc2d5ed25ec9e5fb47
sp <cybersagex@gmail.com>
2018-12-23 15:43:36 +0530

    codemapper question 1 done

--- codemapper/q1.py
+++ codemapper/q1.py
@@ -1,6 +1,6 @@
 def prime_list(l):
     result=[]
-    flag = 0 
+    flag = 0
     for i in l:
         for j in range(2,i):
             if i%j!=0:
@@ -8,8 +8,34 @@ def prime_list(l):
             else:
                 flag = 0
                 break
-        if flag == 1 or i==2 and i not in result:
-            result.append(i)
+        if flag == 1 or i==2 :
+            if i not in result:
+                result.append(i)
     return result
 
+
+# sample testcases
+# test case 1
 print(prime_list([2,3,6,7]))
+# output 1 = [2, 3, 7]
+
+# testcase 2
+print(prime_list([4,6,8,10]))
+# output 2 = []
+
+# testcase 3
+print(prime_list([44,71,356,1032]))
+# output 3 = [71]
+
+# hidden testcases
+# testcase 1
+print(prime_list([2,3,3,6,7,2]))
+# output 1 = [2, 3, 7]
+
+# testcase 2
+print(prime_list([5,6,9,8,5,6,9,8]))
+# output = [5]
+
+# testcase 3
+print(prime_list([]))
+# output 3 = []
