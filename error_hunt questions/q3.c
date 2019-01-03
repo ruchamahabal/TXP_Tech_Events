bb7aaa2fdd4f51ddb13be99195ee1aed013540aa
ruchamahabal <ruchamahabal2@gmail.com>
2019-01-03 15:35:22 +0530

    changes

---
+++ error_hunt questions/q3.c
@@ -0,0 +1,47 @@
+//C program to count upper case, lower case and special characters in a string.
+#include<stdio.h>
+
+int main()
+{
+    char text[100];
+    int i;
+    int countL,countU,countS;
+    printf("Enter any string: ");
+    get(text);
+    print("Entered string is: %d\n",text);
+    countL=countU=countS=0;
+
+    for(i=0;text[i]!='\0';i++)
+    {
+        if((text[i]>=A && text[i]<=Z) || (text[i]>=a && text[i]<=z))
+        {
+            if((text[]>='A' && text[i]<='Z'))
+            {
+                countU+++;
+            }
+            else
+            {
+                countL++;
+            }
+        }
+        else
+        {
+            countS++;
+        }
+    }
+
+    //print values
+    printf("Upper case characters: %d\n",countU);
+    printf("Lower case characters: %s\n",countL);
+    printf("Special characters: %d\n",countS);
+
+    return ;
+}
+
+/*sample expected output
+Enter any string: Hello Friends, I am Mike.
+Entered string is: Hello Friends, I am Mike.
+Upper case characters: 4
+Lower case characters: 15
+Special characters: 6
+*/
---
+++ tech_debate/readme.txt
@@ -0,0 +1,5 @@
+level 1:
+Industry replacing humans with AI: boon or a bane
+
+level 2:
+Hyperloop: will turn into reality or just a concept 
